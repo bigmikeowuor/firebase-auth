a635ce89db60bd67c0ba533d4150c0387fa556a2
Michael Owuor <michael.owuor@gmail.com>
2021-02-24 21:51:05 +0300

    Sign up, in or out with Firebase authentication.

--- Readme.md
+++ Readme.md
@@ -2,6 +2,17 @@
 
 This project was about how to use Firebase Authentication in my Vue.js application and allow users to authenticate with third-party providers (Twitter, GitHub, Google etc.) or plain old email and password. Whats more no server-side code was necessary.
 
+What I learned creating the application:
+
+- How to set up and connect to Firebase Authentication.
+- How to sign users up, in and out.
+- How to catch Firebase auth errors.
+- How to authenticate a user via 3rd party providers.
+- How to update a Firebase user’s profile, email, and password.
+- How to link multiple authentication providers to one user account.
+- How we can store additional user data in our backend.
+- How to protect routes in a Single Page Application, using Vue Router, Vuex, and Firebase Authentication.
+
 ## Live Site
 
 This repository does not have a live site.
---
+++ app.js
@@ -0,0 +1,52 @@
+// firebase configuration
+let firebaseConfig = {
+	apiKey: 'AIzaSyARnOQnc7YLKd8vG-eqCNAtMCX0Cgu5sZQ',
+	authDomain: 'vue-http-demo-mo.firebaseapp.com',
+	databaseURL: 'https://vue-http-demo-mo-default-rtdb.europe-west1.firebasedatabase.app',
+	projectId: 'vue-http-demo-mo',
+	storageBucket: 'vue-http-demo-mo.appspot.com',
+	messagingSenderId: '561930183755',
+	appId: '1:561930183755:web:381946c19b3979e04a19ce',
+};
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// vue.js instance
+var app = new Vue({
+	el: '#app',
+
+	data: {
+		email: '',
+		password: '',
+		authUser: null,
+	},
+
+	methods: {
+		register() {
+			firebase.auth().createUserWithEmailAndPassword(this.email, this.password);
+			this.clearFields();
+		},
+
+		signOut() {
+			firebase.auth().signOut();
+			this.clearFields();
+		},
+
+		signIn() {
+			firebase.auth().signInWithEmailAndPassword(this.email, this.password);
+			this.clearFields();
+		},
+
+		clearFields() {
+			this.email = '';
+			this.password = '';
+		},
+	},
+
+	created() {
+		firebase.auth().onAuthStateChanged((user) => {
+			this.authUser = user;
+		});
+	},
+});
